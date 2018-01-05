7f54c881c1249e1acbf270f77ff3b273888133b3
Magda Balun <magi@op.pl>
2018-01-05 23:54:20 +0100

    Add index.html, index.js, package.json and .jpg files

---
+++ index.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+    if (request.method === 'GET' && (request.url === '/') ) {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        fs.readFile('./index.html', 'utf-8', function(err, data){ 
+            response.write(data);
+            response.end();
+        });
+    } else {
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'image/jpg');
+                
+        fs.readFile('./404.jpg', function(err, data){ 
+            response.write(data);
+            response.end();
+        });
+      }
+});
+
+server.listen(8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
