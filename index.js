37e541dd0566d39ce56dcc5501417fb1f2dc88fa
Magda Balun <magi@op.pl>
2018-01-07 20:51:07 +0100

    Change .js file

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ server.on('request', function (request, response) {
     if (request.method === 'GET' && (request.url === '/') ) {
         response.setHeader('Content-Type', 'text/html; charset=utf-8');
         fs.readFile('./index.html', 'utf-8', function(err, data){ 
+            if(err) throw err;
             response.write(data);
             response.end();
         });
@@ -14,7 +15,8 @@ server.on('request', function (request, response) {
         response.statusCode = 404;
         response.setHeader('Content-Type', 'image/jpg');
                 
-        fs.readFile('./404.jpg', function(err, data){ 
+        fs.readFile('./404.jpg', function(err, data){
+        	if(err) throw err; 
             response.write(data);
             response.end();
         });
